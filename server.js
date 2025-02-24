07c2a6478f8baaa9b03fba475a38474bbbdc6cab
Mahmoud <ashrafmahmoud353@gmail.com>
2025-02-24 13:47:31 +0200

    create APIs

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 import express from "express";
-import connectDB from "./config/db.js"
+import connectDB from "./config/db.js";
+import auth from "./routes/auth.js";
+import profile from "./routes/profile.js";
+import users from "./routes/users.js";
+import posts from "./routes/posts.js";
 
 const app= express();
+
+app.use("/",auth);
+app.use("/api/profile",profile);
+app.use("/api/users",users);
+app.use("/api/posts",posts);
 
 connectDB();
 
@@ -10,4 +19,3 @@ app.get("/", (req,res)=> res.send("Server is working correctly"))
 const PORT= process.env.PORT||5000;
 
 app.listen(PORT,()=> console.log(`Server Has Started At PORT: ${PORT}`))
-// app.use()
